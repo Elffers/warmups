886dfd8414200c2d6597efabcc682f49fec81356
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-03 21:44:07 -0800

    method for converting integer into binary

--- binary.rb
+++ binary.rb
@@ -1,8 +1,24 @@
 class Integer
 
-def toBinary
-  
+  def to_binary
+    exp = exponent_upper_bound(self)
+    num = self
+    binary = []
+    while exp >= 0
+      binary << num / (2**exp)
+      num = num % (2**exp)
+      exp = exp - 1
+    end
+    binary.join("").to_i
+  end
 
-end
+  #return upper bound exponent of 2
+  def exponent_upper_bound(n)
+    upper_bound = 1
+    while  (2**upper_bound) <= n
+      upper_bound += 1
+    end
+    upper_bound - 1
+  end
 
 end
