03880ba535a86aa2caa17d3cd781d322170ee20a
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-05 00:04:19 -0800

    method for converting roman numeral to decimal

--- romannum.rb
+++ romannum.rb
@@ -22,27 +22,60 @@ end
 
 # Second solution
 
-def to_roman(num)
-  numeral_values = [1000,500,100,50,10,5,1]
-  tally = []
-  i = 0
-  dividend = num
-  numeral_values.each_with_index do |value, i|
-    remainder = dividend % value
-    tally << dividend / value
-    dividend = remainder
+  def to_roman(num)
+    numeral_values = [1000,500,100,50,10,5,1]
+    tally = []
+    # i = 0
+    dividend = num
+    numeral_values.each_with_index do |value, i|
+      remainder = dividend % value
+      tally << dividend / value
+      dividend = remainder
+    end
+    sym = %w(M, D, C, L, X, V, I)
+    roman_string = ""
+    tally.zip(sym) {|count, symbol| roman_string << symbol * count }
+    old_rn = roman_string.scan(/\w/).join("")
+
+    # old_rn.gsub!(/DCCCC/, 'CM')
+    # old_rn.gsub(/CCCC/, 'CL')
+    # old_rn.gsub(/LXXXX/, 'XC')
+    # old_rn.gsub(/XXXX/, 'XL')
   end
-  sym = %w(M, D, C, L, X, V, I)
-  roman_string = ""
-  tally.zip(sym) {|count, symbol| roman_string << symbol * count }
-  old_rn = roman_string.scan(/\w/).join("")
 
-  # old_rn.gsub!(/DCCCC/, 'CM')
-  # old_rn.gsub(/CCCC/, 'CL')
-  # old_rn.gsub(/LXXXX/, 'XC')
-  # old_rn.gsub(/XXXX/, 'XL')
+
+# Convert from Roman numeral
+# e.g. "MCCCXLIIII" = 1344
+
+def from_roman(str)
+  hash = {}
+  str.each_char do |letter|
+    hash.default = 0
+    hash[rn_to_decimal(letter)] += 1 
   end
+  array = []
+  hash.each do |k, v|
+    array << k*v
+  end
+  array.reduce(:+)
+end
 
-  # Convert from Roman numeral
-  # e.g. "MCCCXLIIII" = 1344
-
+def rn_to_decimal(letter)
+  case letter
+  when "M"
+    value = 1000
+  when "D"
+    value = 500
+  when "C"
+    value = 100
+  when "L"
+    value = 50
+  when "X"
+    value = 10
+  when "V"
+    value = 5
+  when "I"
+    value = 1
+  end
+  value
+end
