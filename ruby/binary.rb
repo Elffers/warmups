8fa4623451e04d6e785cad8d38ce19beb2f86300
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-30 11:22:19 -0700

    move to ruby folder

--- ruby/binary.rb
+++ ruby/binary.rb
@@ -1,7 +1,6 @@
 class Integer
-
   def to_binary
-    exp = Math.log2(self).to_i 
+    exp = Math.log2(self).to_i
     num = self
     binary = []
     while exp >= 0
@@ -12,7 +11,7 @@ class Integer
     binary.join("").to_i
   end
 
-  #return upper bound exponent of 2. Replaced with Math.log2(n)
+  # return upper bound exponent of 2. Replaced with Math.log2(n)
   def exponent_upper_bound(n)
     upper_bound = 1
     while  (2**upper_bound) <= n
@@ -24,7 +23,7 @@ end
 
 # as isolated method
 def to_binary(num)
-  exp = Math.log2(num).to_i 
+  exp = Math.log2(num).to_i
   binary = []
   while exp >= 0
     binary << num / (2**exp)
@@ -33,3 +32,24 @@ def to_binary(num)
   end
   binary.join("").to_i
 end
+
+# Using division by 2
+def toBinary(decimal)
+  binary_string = ""
+  while decimal > 0
+    remainder = decimal % 2
+    binary_string.concat(remainder.to_s)
+    decimal = decimal / 2
+  end
+  binary_string.reverse
+end
+
+
+
+
+
+
+
+
+
+
