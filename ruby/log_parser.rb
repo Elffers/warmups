335d48a5ac9476cd17c5f680c480b0c76cfef59c
Hsing-Hui Hsu <hsing-hui@carezone.com>
2014-11-25 01:07:26 -0800

    Platinum works

--- ruby/log_parser.rb
+++ ruby/log_parser.rb
@@ -8,12 +8,15 @@ class LogParser
   end
 
   def parse
-    request_counter = Hash.new 0 
+    request_counter = Hash.new do |hash, key|
+      hash[key] = Hash.new 0
+    end
+
     File.open filename do |f|
       while not f.eof
         line = f.gets
-        if /Started\s(?<request_type>[A-Z]+)\s(?<path>".+")\s(.+at\s)(?<timestamp>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2})/=~ line
-          request_counter[timestamp] += 1
+        if /Started\s(?<request_type>[A-Z]+)\s"(?<path>.+)"\s(.+at\s)(?<timestamp>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2})/=~ line
+          request_counter[timestamp][path] += 1
         end
       end
     end
--- ruby/log_parser_spec.rb
+++ ruby/log_parser_spec.rb
@@ -1,14 +1,15 @@
 require_relative 'log_parser'
 
 describe LogParser do
-  let(:sample) { "sample_log_small.txt"}
-  let(:log_parser) { LogParser.new sample }
+  let(:sample1) { "sample_log_small.txt"}
+  let(:log_parser_small) { LogParser.new sample1 }
+  let(:sample2) { "sample_log_large.txt"}
+  let(:log_parser_large) { LogParser.new sample2 }
+
   context 'parses logfile' do
     it 'returns total number of requests' do
-      expect(log_parser.parse).to eq ({"2014-10-23 08:37"=>1})
-      large_log = LogParser.new "sample_log_large.txt"
-      expect(large_log.parse).to eq ({"2014-10-23 08:37"=>22, "2014-10-23 08:38"=>978, "2014-10-23 08:59"=>63, "2014-10-23 09:00"=>117, "2014-10-23 09:01"=>20})
+      expect(log_parser_small.parse).to eq ({"2014-10-23 08:37"=>{"/posts/new"=>1}})
+      expect(log_parser_large.parse).to eq ({"2014-10-23 08:37"=>{"/posts/new"=>10, "/posts"=>6, "/posts/9/edit"=>3, "/"=>3}, "2014-10-23 08:38"=>{"/posts/9/edit"=>234, "/"=>222, "/posts"=>250, "/posts/new"=>272}, "2014-10-23 08:59"=>{"/"=>17, "/posts/new"=>14, "/posts/9/edit"=>17, "/posts"=>15}, "2014-10-23 09:00"=>{"/"=>32, "/posts/new"=>30, "/posts/9/edit"=>31, "/posts"=>24}, "2014-10-23 09:01"=>{"/posts/new"=>5, "/posts/9/edit"=>5, "/"=>4, "/posts"=>6}})
     end
-
   end
 end
